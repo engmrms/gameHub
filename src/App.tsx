bf962aac486226c105f93061d5132c79fb48a778
mohammad rabah <mrabah@tetco.sa>
2023-04-17 00:38:57 +0300

    responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import { useState } from "react";
 import "./App.css";
 
@@ -6,11 +6,23 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <Button colorScheme="teal" size={"sm"}>
-        Click Me
-      </Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}>
+      <GridItem area={"nav"} bg={"orange.300"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"green.300"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"teal.300"}>
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
