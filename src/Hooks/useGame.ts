30b644919bed0be8398696fe1f4705c596d38abf
mohammad rabah <mrabah@tetco.sa>
2023-04-17 11:18:15 +0300

    GameCard

--- src/Hooks/useGame.ts
+++ src/Hooks/useGame.ts
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import apiClient, { CanceledError } from "../Services/api-client";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 interface FetchGameResponse {
   count: number;
