574aca83af637d47a180e6f290388a0853eff27e
mohammad rabah <mrabah@tetco.sa>
2023-04-18 19:42:50 +0300

    customize themes

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
   initialColorMode: "dark",
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#f2f2f2",
+      100: "#d9d9d9",
+      200: "#bfbfbf",
+      300: "#a6a6a6",
+      400: "#8c8c8c",
+      500: "#737373",
+      600: "#595959",
+      700: "#202020",
+      800: "#121212",
+      900: "#111",
+    },
+  },
+});
 
 export default theme;
